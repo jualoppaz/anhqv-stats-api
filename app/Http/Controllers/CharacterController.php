7c5772a93c7d755b35451f3041197ade0d99f2c3
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-21 19:02:59 +0100

    Implementación de recurso GET /characters con estrategia de repositorios

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -2,21 +2,68 @@
 
 namespace App\Http\Controllers;
 
-use App\Character;
+use Anhqv\Character\CharacterRepoInterface;
 use Illuminate\Http\Request;
+use Utils;
 
 class CharacterController extends Controller
 {
+    // Log levels
+    const LOG_LEVEL_INFO = LOG_LEVEL_INFO;
+    const LOG_LEVEL_DEBUG = LOG_LEVEL_DEBUG;
+
+    // HTTP status codes
+    const HTTP_OK = HTTP_OK;
+
+    // Fields
+    const ID = 'id';
+    const NAME = 'name';
+    const SURNAME = 'surname';
+    const NICKNAME = 'nickname';
+    const PAGE = 'page';
+    const PER_PAGE = 'per_page';
+
+    protected $rules_index = [
+        self::NAME => 'sometimes|required',
+        self::PAGE => 'required_with:per_page|integer|min:1',
+        self::PER_PAGE => 'required_with:page|integer|min:1',
+    ];
+
+    public function __construct(CharacterRepoInterface $characterRepo)
+    {
+        $this->className = class_basename($this);
+
+        $this->characterRepo = $characterRepo;
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $characters = Character::get();
+        $methodName = __FUNCTION__;
+        Utils::log(static::LOG_LEVEL_DEBUG, $this->className, $methodName, 'Access');
 
-        return response()->json($characters, 200, [], JSON_PRETTY_PRINT);
+        $data = $request->validate($this->rules_index);
+
+        $name = isset($data[static::NAME]) ? $data[static::NAME] : null;
+        $page = isset($data[static::PAGE]) ? $data[static::PAGE] : 1;
+        $perPage = isset($data[static::PER_PAGE]) ? $data[static::PER_PAGE] : 10;
+
+        $filters = [
+            static::NAME => $name,
+        ];
+
+        $visibleFields = [
+            static::ID, static::NAME, static::SURNAME, static::NICKNAME,
+        ];
+
+        $res = $this->characterRepo->getPaginated($request, $filters, $page, $perPage, $visibleFields);
+
+        Utils::log(static::LOG_LEVEL_DEBUG, $this->className, $methodName, 'Exit');
+        return response()->json($res, static::HTTP_OK, [], JSON_PRETTY_PRINT);
     }
 
     /**
