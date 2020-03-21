669671b5a4466c489e4762caed5d12e22a0c2da7
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-21 18:41:07 +0100

    Implementación del repositorio para el modelo Character

---
+++ app/Anhqv/Character/CharacterRepo.php
@@ -0,0 +1,61 @@
+<?php namespace Anhqv\Character;
+
+use Anhqv\Base\BaseRepo;
+use Illuminate\Http\Request;
+
+class CharacterRepo extends BaseRepo implements CharacterRepoInterface
+{
+
+    protected $model;
+
+    public function __construct()
+    {
+        $this->model = new Character;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    public function setModel(Character $character)
+    {
+        $this->model = $character;
+        return $this->model;
+    }
+
+    public function getPaginated(Request $request, $filters, $page, $perPage, array $visibleFields)
+    {
+
+        $query = $this->getModel();
+
+        // Aplicamos los filtros
+
+        $name = $filters['name'];
+
+        if (isset($name)) {
+            $query = $query->where('name', 'like', '%' . $name . '%');
+        }
+
+        // Aplicamos la paginacion
+        $query = $query
+            ->paginate($perPage, ['*'], 'page', $page)
+            ->appends($request->except('pagina'));
+
+        // El map hay que hacerlo en el setCollection para que respete la estructura de la respuesta construida por el paginate
+        /*$query->setCollection(
+            $query->getCollection()
+                ->map(function ($item, $key) use ($visibleFields) {
+
+                    $item->[relationship]];
+
+                    $item->setVisible($visibleFields);
+
+                    return $item;
+                })
+        );*/
+
+        return $query;
+
+    }
+}
