05ae8cfda168a9ea6f1fd2f06ac4fc3f55199377
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-21 13:51:35 +0100

    Commit inicial

---
+++ database/migrations/2020_03_21_113531_create_characters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharactersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->string('surname', 50);
+            $table->string('nickname', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
