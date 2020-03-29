115536164ab46a4124ef88a01c71d8b0f2c4742c
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-29 18:48:39 +0200

    #1 Actualización del seeder de personajes con la columna slug

--- database/seeds/CharactersTableSeeder.php
+++ database/seeds/CharactersTableSeeder.php
@@ -12,6 +12,7 @@ class CharactersTableSeeder extends Seeder
     const FIELD_SECOND_SURNAME = 'second_surname';
     const FIELD_NICKNAME = 'nickname';
     const FIELD_IMAGE_URL = 'image_url';
+    const FIELD_SLUG = 'slug';
 
     /**
      * Run the database seeds.
@@ -30,6 +31,7 @@ class CharactersTableSeeder extends Seeder
                 static::FIELD_SECOND_SURNAME => $character[static::FIELD_SECOND_SURNAME],
                 static::FIELD_NICKNAME => $character[static::FIELD_NICKNAME],
                 static::FIELD_IMAGE_URL => $character[static::FIELD_IMAGE_URL],
+                static::FIELD_SLUG => $character[static::FIELD_SLUG],
             ]);
         }
     }
@@ -42,139 +44,162 @@ class CharactersTableSeeder extends Seeder
                 static::FIELD_SURNAME => 'Benito',
                 static::FIELD_SECOND_SURNAME => 'Valbuena',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/marisa.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/marisa.jpg',
+                static::FIELD_SLUG => 'marisa'
             ], [
                 static::FIELD_NAME => 'Vicenta',
                 static::FIELD_SURNAME => 'Benito',
                 static::FIELD_SECOND_SURNAME => 'Valbuena',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/vicenta.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/vicenta.jpg',
+                static::FIELD_SLUG => 'vicenta'
             ], [
                 static::FIELD_NAME => 'Juan',
                 static::FIELD_SURNAME => 'Cuesta',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/juan_cuesta.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/juan_cuesta.jpg',
+                static::FIELD_SLUG => 'juan-cuesta'
             ], [
                 static::FIELD_NAME => 'Paloma',
                 static::FIELD_SURNAME => 'Hurtado',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/paloma.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/paloma.jpg',
+                static::FIELD_SLUG => 'paloma'
             ], [
                 static::FIELD_NAME => 'José Miguel',
                 static::FIELD_SURNAME => 'Cuesta',
                 static::FIELD_SECOND_SURNAME => 'Hurtado',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/josemi.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/josemi.jpg',
+                static::FIELD_SLUG => 'josemi'
             ], [
                 static::FIELD_NAME => 'Belén',
                 static::FIELD_SURNAME => 'López',
                 static::FIELD_SECOND_SURNAME => 'Vázquez',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/belen.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/belen.jpg',
+                static::FIELD_SLUG => 'belen'
             ], [
                 static::FIELD_NAME => 'Alicia',
                 static::FIELD_SURNAME => 'Sanz',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/alicia.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/alicia.jpg',
+                static::FIELD_SLUG => 'alicia'
             ], [
                 static::FIELD_NAME => 'Concepción',
                 static::FIELD_SURNAME => '',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/concha.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/concha.jpg',
+                static::FIELD_SLUG => 'concha'
             ], [
                 static::FIELD_NAME => 'Daniel',
                 static::FIELD_SURNAME => 'Rubio',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/dani.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/dani.jpg',
+                static::FIELD_SLUG => 'dani'
             ], [
                 static::FIELD_NAME => 'Armando',
                 static::FIELD_SURNAME => 'Rubio',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/armando.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/armando.jpg',
+                static::FIELD_SLUG => 'armando'
             ], [
                 static::FIELD_NAME => 'Mauricio',
                 static::FIELD_SURNAME => 'Hidalgo',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/mauri.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/mauri.jpg',
+                static::FIELD_SLUG => 'mauri'
             ], [
                 static::FIELD_NAME => 'Fernando',
                 static::FIELD_SURNAME => 'Navarro',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/fernando.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/fernando.jpg',
+                static::FIELD_SLUG => 'fernando'
             ], [
                 static::FIELD_NAME => 'Natalia',
                 static::FIELD_SURNAME => 'Cuesta',
                 static::FIELD_SECOND_SURNAME => 'Hurtado',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/natalia.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/natalia.jpg',
+                static::FIELD_SLUG => 'natalia'
             ], [
                 static::FIELD_NAME => 'Roberto',
                 static::FIELD_SURNAME => 'Alonso',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/roberto.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/roberto.jpg',
+                static::FIELD_SLUG => 'roberto'
             ], [
                 static::FIELD_NAME => 'Lucía',
                 static::FIELD_SURNAME => 'Álvarez',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => 'La pija',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/lucia.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/lucia.jpg',
+                static::FIELD_SLUG => 'lucia'
             ], [
                 static::FIELD_NAME => 'Emilio',
                 static::FIELD_SURNAME => 'Delgado',
                 static::FIELD_SECOND_SURNAME => 'Martínez',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/emilio.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/emilio.jpg',
+                static::FIELD_SLUG => 'emilio'
             ], [
                 static::FIELD_NAME => 'Mozo mudanza 1', // Antonio gomez
                 static::FIELD_SURNAME => '',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/mozo_mudanza_1.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/mozo_mudanza_1.jpg',
+                static::FIELD_SLUG => 'mozo-mudanza-1'
             ], [
                 static::FIELD_NAME => 'Mozo mudanza 2', // Arsenio Luna (el negrucio)
                 static::FIELD_SURNAME => '',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/mozo_mudanza_2.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/mozo_mudanza_2.jpg',
+                static::FIELD_SLUG => 'mozo-mudanza-2'
             ], [
                 static::FIELD_NAME => 'Repartidor propaganda',
                 static::FIELD_SURNAME => '',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/repartidor_propaganda.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/repartidor_propaganda.jpg',
+                static::FIELD_SLUG => 'repartidor-propaganda'
             ], [
                 static::FIELD_NAME => 'Paco',
                 static::FIELD_SURNAME => '',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/paco.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/paco.jpg',
+                static::FIELD_SLUG => 'paco'
             ], [
                 static::FIELD_NAME => 'Santiago',
                 static::FIELD_SURNAME => 'Segura',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/santiago_segura.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/santiago_segura.jpg',
+                static::FIELD_SLUG => 'santiago-segura'
             ], [
                 static::FIELD_NAME => 'Mariano',
                 static::FIELD_SURNAME => 'Delgado',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/mariano.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/mariano.jpg',
+                static::FIELD_SLUG => 'mariano'
             ], [
                 static::FIELD_NAME => 'Esther',  // Susana Reija
                 static::FIELD_SURNAME => '',
                 static::FIELD_SECOND_SURNAME => '',
                 static::FIELD_NICKNAME => '',
-                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/esther.jpg'
+                static::FIELD_IMAGE_URL => 'http://anhqv-stats.es/images/characters/esther.jpg',
+                static::FIELD_SLUG => 'esther'
             ]
         ];
     }
