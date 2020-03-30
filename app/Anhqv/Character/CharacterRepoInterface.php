007dac8df1686540c008822dd8a337b39571c70d
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-30 20:35:52 +0200

    #5 Definición de método findBySlug en la interfaz de Personajes

--- app/Anhqv/Character/CharacterRepoInterface.php
+++ app/Anhqv/Character/CharacterRepoInterface.php
@@ -4,5 +4,10 @@ use Anhqv\Base\BaseRepoInterface;
 
 interface CharacterRepoInterface extends BaseRepoInterface
 {
-
+    /**
+     * Method for get one model by the given slug.
+     *
+     * @author jualoppaz
+     */
+    public function findBySlug($slug);
 }
