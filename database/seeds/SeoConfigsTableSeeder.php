d7e9555f7f417b9c556e0e17aba5661bda8f9648
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-04-29 19:17:39 +0200

    #22 Definición de configuración de SEO para el capítulo 1x15

--- database/seeds/SeoConfigsTableSeeder.php
+++ database/seeds/SeoConfigsTableSeeder.php
@@ -806,6 +806,18 @@ class SeoConfigsTableSeeder extends Seeder
         static::FIELD_TWITTER_CARD => static::VALUE_TWITTER_CARD,
         static::FIELD_TWITTER_SITE => static::VALUE_TWITTER_SITE,
       ], [
+        static::FIELD_SLUG => '1x15-erase-un-anillo',
+        static::FIELD_TITLE => '1x15 | Érase un anillo',
+        static::FIELD_DESCRIPTION => 'Ficha con los detalles del capítulo 1x15 (Érase un anillo) de Aquí No Hay Quien Viva.',
+        static::FIELD_CANONICAL_URL => 'http://anhqv-stats.es/temporadas/1/capitulos/1x15-erase-un-anillo',
+        static::FIELD_OG_TITLE => '1x15 | Érase un anillo',
+        static::FIELD_OG_TYPE => static::VALUE_OG_TYPE,
+        static::FIELD_OG_IMAGE => 'http://anhqv-stats.es/images/chapters/1x15.jpg',
+        static::FIELD_OG_URL => 'http://anhqv-stats.es/temporadas/1/capitulos/1x15-erase-un-anillo',
+        static::FIELD_OG_DESCRIPTION => 'Ficha con los detalles del capítulo 1x15 (Érase un anillo) de Aquí No Hay Quien Viva.',
+        static::FIELD_TWITTER_CARD => static::VALUE_TWITTER_CARD,
+        static::FIELD_TWITTER_SITE => static::VALUE_TWITTER_SITE,
+      ], [
         static::FIELD_SLUG => 'season-2-chapters',
         static::FIELD_TITLE => 'Temporada 2 | Capítulos',
         static::FIELD_DESCRIPTION => 'Galería con todos los capítulos de la segunda temporada de Aquí No Hay Quien Viva.',
