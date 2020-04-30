f7597c1dc52cb5f1e9afc0262684b6f1e7b1771f
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-04-30 23:21:19 +0200

    #22 Definición de configuración de SEO para el capítulo 2x11

--- database/seeds/SeoConfigsTableSeeder.php
+++ database/seeds/SeoConfigsTableSeeder.php
@@ -974,6 +974,18 @@ class SeoConfigsTableSeeder extends Seeder
         static::FIELD_TWITTER_CARD => static::VALUE_TWITTER_CARD,
         static::FIELD_TWITTER_SITE => static::VALUE_TWITTER_SITE,
       ], [
+        static::FIELD_SLUG => '2x11-erase-unas-elecciones',
+        static::FIELD_TITLE => '2x11 | Érase unas elecciones',
+        static::FIELD_DESCRIPTION => 'Ficha con los detalles del capítulo 2x11 (Érase unas elecciones) de Aquí No Hay Quien Viva.',
+        static::FIELD_CANONICAL_URL => 'http://anhqv-stats.es/temporadas/2/capitulos/2x11-erase-unas-elecciones',
+        static::FIELD_OG_TITLE => '2x11 | Érase unas elecciones',
+        static::FIELD_OG_TYPE => static::VALUE_OG_TYPE,
+        static::FIELD_OG_IMAGE => 'http://anhqv-stats.es/images/chapters/2x11.jpg',
+        static::FIELD_OG_URL => 'http://anhqv-stats.es/temporadas/2/capitulos/2x11-erase-unas-elecciones',
+        static::FIELD_OG_DESCRIPTION => 'Ficha con los detalles del capítulo 2x11 (Érase unas elecciones) de Aquí No Hay Quien Viva.',
+        static::FIELD_TWITTER_CARD => static::VALUE_TWITTER_CARD,
+        static::FIELD_TWITTER_SITE => static::VALUE_TWITTER_SITE,
+      ], [
         static::FIELD_SLUG => 'season-3-chapters',
         static::FIELD_TITLE => 'Temporada 3 | Capítulos',
         static::FIELD_DESCRIPTION => 'Galería con todos los capítulos de la tercera temporada de Aquí No Hay Quien Viva.',
