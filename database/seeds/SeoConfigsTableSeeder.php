8940c39c46689e8a28bf337166b3f57ce4dac930
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-04-30 23:09:03 +0200

    #22 Definición de configuración de SEO para el capítulo 2x09

--- database/seeds/SeoConfigsTableSeeder.php
+++ database/seeds/SeoConfigsTableSeeder.php
@@ -950,6 +950,18 @@ class SeoConfigsTableSeeder extends Seeder
         static::FIELD_TWITTER_CARD => static::VALUE_TWITTER_CARD,
         static::FIELD_TWITTER_SITE => static::VALUE_TWITTER_SITE,
       ], [
+        static::FIELD_SLUG => '2x09-erase-una-parabolica',
+        static::FIELD_TITLE => '2x09 | Érase una parabólica',
+        static::FIELD_DESCRIPTION => 'Ficha con los detalles del capítulo 2x09 (Érase una parabólica) de Aquí No Hay Quien Viva.',
+        static::FIELD_CANONICAL_URL => 'http://anhqv-stats.es/temporadas/2/capitulos/2x09-erase-una-parabolica',
+        static::FIELD_OG_TITLE => '2x09 | Érase una parabólica',
+        static::FIELD_OG_TYPE => static::VALUE_OG_TYPE,
+        static::FIELD_OG_IMAGE => 'http://anhqv-stats.es/images/chapters/2x09.jpg',
+        static::FIELD_OG_URL => 'http://anhqv-stats.es/temporadas/2/capitulos/2x09-erase-una-parabolica',
+        static::FIELD_OG_DESCRIPTION => 'Ficha con los detalles del capítulo 2x09 (Érase una parabólica) de Aquí No Hay Quien Viva.',
+        static::FIELD_TWITTER_CARD => static::VALUE_TWITTER_CARD,
+        static::FIELD_TWITTER_SITE => static::VALUE_TWITTER_SITE,
+      ], [
         static::FIELD_SLUG => 'season-3-chapters',
         static::FIELD_TITLE => 'Temporada 3 | Capítulos',
         static::FIELD_DESCRIPTION => 'Galería con todos los capítulos de la tercera temporada de Aquí No Hay Quien Viva.',
