50dc75925c30e08d5ec53967c04fd535c647731e
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-05-01 17:13:18 +0200

    #22 Definición de configuración de SEO para el capítulo 5x13

--- database/seeds/SeoConfigsTableSeeder.php
+++ database/seeds/SeoConfigsTableSeeder.php
@@ -1789,6 +1789,18 @@ class SeoConfigsTableSeeder extends Seeder
         static::FIELD_OG_DESCRIPTION => 'Ficha con los detalles del capítulo 5x12 (Érase un paripé) de Aquí No Hay Quien Viva.',
         static::FIELD_TWITTER_CARD => static::VALUE_TWITTER_CARD,
         static::FIELD_TWITTER_SITE => static::VALUE_TWITTER_SITE,
+      ], [
+        static::FIELD_SLUG => '5x13-erase-un-adios',
+        static::FIELD_TITLE => '5x13 | Érase un adiós',
+        static::FIELD_DESCRIPTION => 'Ficha con los detalles del capítulo 5x13 (Érase un adiós) de Aquí No Hay Quien Viva.',
+        static::FIELD_CANONICAL_URL => 'http://anhqv-stats.es/temporadas/5/capitulos/5x13-erase-un-adios',
+        static::FIELD_OG_TITLE => '5x13 | Érase un adiós',
+        static::FIELD_OG_TYPE => static::VALUE_OG_TYPE,
+        static::FIELD_OG_IMAGE => 'http://anhqv-stats.es/images/chapters/5x13.jpg',
+        static::FIELD_OG_URL => 'http://anhqv-stats.es/temporadas/5/capitulos/5x13-erase-un-adios',
+        static::FIELD_OG_DESCRIPTION => 'Ficha con los detalles del capítulo 5x13 (Érase un adiós) de Aquí No Hay Quien Viva.',
+        static::FIELD_TWITTER_CARD => static::VALUE_TWITTER_CARD,
+        static::FIELD_TWITTER_SITE => static::VALUE_TWITTER_SITE,
       ]
     ];
   }
