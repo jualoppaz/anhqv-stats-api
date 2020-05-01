91937a5512346e9eca26434e386f636a9e636093
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-05-01 12:55:18 +0200

    #22 Definición de configuración de SEO para el capítulo 3x26

--- database/seeds/SeoConfigsTableSeeder.php
+++ database/seeds/SeoConfigsTableSeeder.php
@@ -1346,6 +1346,18 @@ class SeoConfigsTableSeeder extends Seeder
         static::FIELD_TWITTER_CARD => static::VALUE_TWITTER_CARD,
         static::FIELD_TWITTER_SITE => static::VALUE_TWITTER_SITE,
       ], [
+        static::FIELD_SLUG => '3x26-erase-el-primer-presidente-gay',
+        static::FIELD_TITLE => '3x26 | Érase el primer presidente gay',
+        static::FIELD_DESCRIPTION => 'Ficha con los detalles del capítulo 3x26 (Érase el primer presidente gay) de Aquí No Hay Quien Viva.',
+        static::FIELD_CANONICAL_URL => 'http://anhqv-stats.es/temporadas/3/capitulos/3x26-erase-el-primer-presidente-gay',
+        static::FIELD_OG_TITLE => '3x26 | Érase el primer presidente gay',
+        static::FIELD_OG_TYPE => static::VALUE_OG_TYPE,
+        static::FIELD_OG_IMAGE => 'http://anhqv-stats.es/images/chapters/3x26.jpg',
+        static::FIELD_OG_URL => 'http://anhqv-stats.es/temporadas/3/capitulos/3x26-erase-el-primer-presidente-gay',
+        static::FIELD_OG_DESCRIPTION => 'Ficha con los detalles del capítulo 3x26 (Érase el primer presidente gay) de Aquí No Hay Quien Viva.',
+        static::FIELD_TWITTER_CARD => static::VALUE_TWITTER_CARD,
+        static::FIELD_TWITTER_SITE => static::VALUE_TWITTER_SITE,
+      ], [
         static::FIELD_SLUG => 'season-4-chapters',
         static::FIELD_TITLE => 'Temporada 4 | Capítulos',
         static::FIELD_DESCRIPTION => 'Galería con todos los capítulos de la cuarta temporada de Aquí No Hay Quien Viva.',
