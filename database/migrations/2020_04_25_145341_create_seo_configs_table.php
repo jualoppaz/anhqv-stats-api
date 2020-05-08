7e08ee0b1751cfce3400397598e37ca8c995c083
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-05-08 22:18:46 +0200

    #26 Creación de nuevas columnas para meta etiquetas de OpenGraph y Twitter en la tabla seo-configs

--- database/migrations/2020_04_25_145341_create_seo_configs_table.php
+++ database/migrations/2020_04_25_145341_create_seo_configs_table.php
@@ -12,13 +12,21 @@ class CreateSeoConfigsTable extends Migration
   const FIELD_TITLE = 'title';
   const FIELD_DESCRIPTION = 'description';
   const FIELD_CANONICAL_URL = 'canonical_url';
+
+  // Open Graph fields
   const FIELD_OG_TITLE = 'og_title';
   const FIELD_OG_TYPE = 'og_type';
   const FIELD_OG_IMAGE = 'og_image';
   const FIELD_OG_URL = 'og_url';
   const FIELD_OG_DESCRIPTION = 'og_description';
+  const FIELD_OG_SITE_NAME = 'og_site_name';
+
+  // Twitter fields
   const FIELD_TWITTER_CARD = 'twitter_card';
   const FIELD_TWITTER_SITE = 'twitter_site';
+  const FIELD_TWITTER_IMAGE = 'twitter_image';
+  const FIELD_TWITTER_TITLE = 'twitter_title';
+  const FIELD_TWITTER_DESCRIPTION = 'twitter_description';
 
   /**
    * Run the migrations.
@@ -33,13 +41,17 @@ class CreateSeoConfigsTable extends Migration
       $table->string(static::FIELD_TITLE)->comment('Título de la página');
       $table->string(static::FIELD_DESCRIPTION)->comment('Descripción deseada en buscadores');
       $table->string(static::FIELD_CANONICAL_URL)->comment('Url absoluta que identifica a la página de forma inequívoca');
-      $table->string(static::FIELD_OG_TITLE)->comment('Título para el meta og:title de open graph');
-      $table->string(static::FIELD_OG_TYPE)->comment('Tipo de contenido para el meta og:type de open graph');
-      $table->string(static::FIELD_OG_IMAGE)->comment('Url de la imagen deseada en el meta og:image de open graph');
-      $table->string(static::FIELD_OG_URL)->comment('Url del contenido posicionado en el meta og:url de open graph');
-      $table->string(static::FIELD_OG_DESCRIPTION)->comment('Descripción para el meta og:description de open graph');
+      $table->string(static::FIELD_OG_TITLE)->comment('Título para el meta og:title de');
+      $table->string(static::FIELD_OG_TYPE)->comment('Tipo de contenido para el meta og:type');
+      $table->string(static::FIELD_OG_IMAGE)->comment('Url de la imagen deseada en el meta og:image');
+      $table->string(static::FIELD_OG_URL)->comment('Url del contenido posicionado en el meta og:url');
+      $table->string(static::FIELD_OG_DESCRIPTION)->comment('Descripción para el meta og:description');
+      $table->string(static::FIELD_OG_SITE_NAME)->comment('Nombre del sitio web');
       $table->string(static::FIELD_TWITTER_CARD)->comment('Tipo de tarjeta para el meta twitter:card');
       $table->string(static::FIELD_TWITTER_SITE)->comment('Nombre de la web para el meta twitter:site');
+      $table->string(static::FIELD_TWITTER_IMAGE)->comment('Url de la imagen deseada en el meta twitter:image');
+      $table->string(static::FIELD_TWITTER_TITLE)->comment('Título para el meta twitter:title');
+      $table->string(static::FIELD_TWITTER_DESCRIPTION)->comment('Descripción para el meta twitter:description');
 
       // constraints
       $table->unique(static::FIELD_SLUG);
