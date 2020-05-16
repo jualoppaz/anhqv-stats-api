5dfad80f0e4d806941486a870c91d596da05e5ac
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-05-17 00:12:46 +0200

    #35 Creación de columna title en la tabla de escenas

--- database/migrations/2020_05_10_182010_create_scenes_table.php
+++ database/migrations/2020_05_10_182010_create_scenes_table.php
@@ -13,6 +13,7 @@ class CreateScenesTable extends Migration
   const FIELD_IMAGE_URL = 'image_url';
   const FIELD_IMAGE_ALT = 'image_alt';
   const FIELD_CHAPTER_ID = 'chapter_id';
+  const FIELD_TITLE = 'title';
   const FIELD_ORDER = 'order';
   const FIELD_ID = 'id';
 
@@ -29,6 +30,7 @@ class CreateScenesTable extends Migration
       $table->string(static::FIELD_IMAGE_URL, 100)->comment('Url a la imagen de la escena');
       $table->string(static::FIELD_IMAGE_ALT, 100)->comment('Texto a mostrar en el atributo alt de la imagen de la escena');
       $table->bigInteger(static::FIELD_CHAPTER_ID)->comment('Id del capítulo al que pertenece la escena');
+      $table->string(static::FIELD_TITLE)->nullable(false)->comment('Título que resumen la escena');
       $table->integer(static::FIELD_ORDER)->comment('Orden de la escena en el capítulo');
 
       $table->foreign(static::FIELD_CHAPTER_ID)->references(static::FIELD_ID)->on(static::TABLE_CHAPTERS_NAME);
