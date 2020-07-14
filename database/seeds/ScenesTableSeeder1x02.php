c7c15709b7eb8931f5bfe0d088b00ede19ef5292
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-07-14 23:30:27 +0200

    #43 Información de primera escena del capítulo 1x02

---
+++ database/seeds/ScenesTableSeeder1x02.php
@@ -0,0 +1,37 @@
+<?php
+
+class ScenesTableSeeder1x02
+{
+
+  const TABLE_NAME = 'scenes';
+  const FIELD_IMAGE_URL = 'image_url';
+  const FIELD_IMAGE_ALT = 'image_alt';
+  const FIELD_CHAPTER_ID = 'chapter_id';
+  const FIELD_TITLE = 'title';
+  const FIELD_ORDER = 'order';
+
+  public function __construct()
+  {
+
+  }
+
+  /**
+   * Escenas del capítulo 1x02
+   */
+  public function getScenes_1x02()
+  {
+    $scenes = [];
+
+    $chapter_1x02 = DB::table('chapters')->whereNaturalId('1x02')->first()->id;
+
+    $scenes[] = [
+      static::FIELD_IMAGE_URL => '',
+      static::FIELD_IMAGE_ALT => '',
+      static::FIELD_CHAPTER_ID => $chapter_1x02,
+      static::FIELD_TITLE => 'Marisa y Vicenta se preparan para la capea',
+      static::FIELD_ORDER => count($scenes) + 1,
+    ];
+
+    return $scenes;
+  }
+}
