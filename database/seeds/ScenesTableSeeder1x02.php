2b498dcee5d416d7cd0f3c4044effe7c627d6858
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-07-22 17:11:28 +0200

    #43 Información de la escena 4 del capítulo 1x02

--- database/seeds/ScenesTableSeeder1x02.php
+++ database/seeds/ScenesTableSeeder1x02.php
@@ -48,6 +48,14 @@ class ScenesTableSeeder1x02
       static::FIELD_ORDER => count($scenes) + 1,
     ];
 
+    $scenes[] = [
+      static::FIELD_IMAGE_URL => '',
+      static::FIELD_IMAGE_ALT => '',
+      static::FIELD_CHAPTER_ID => $chapter_1x02,
+      static::FIELD_TITLE => 'Natalia flirtea con Roberto',
+      static::FIELD_ORDER => count($scenes) + 1,
+    ];
+
     return $scenes;
   }
 }
